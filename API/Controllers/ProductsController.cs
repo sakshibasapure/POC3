0187af9a016cd65ecc52e389d1c7cb090911430f
Sakshi Basapure <sakshibasapure99@gmail.com>
2021-03-29 20:27:27 +0530

    Generalization, Specification, AutoMappers added

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase 
+    {
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<Category> _categoriesRepo;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> productsRepo, 
+            IGenericRepository<Category> categoriesRepo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _productsRepo = productsRepo;
+            _categoriesRepo = categoriesRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        {
+            var spec = new ProductCategoriesSpecification();
+
+            var products = await _productsRepo.ListAsync(spec);
+
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductCategoriesSpecification(id);
+
+            var product =  await _productsRepo.GetEnitityWithSpec(spec);
+
+            return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
+
+
+        }
+
+        [HttpGet("categories")]
+        public async Task<ActionResult<IReadOnlyList<Category>>> GetCategories()
+        {
+            return Ok(await _categoriesRepo.ListAllAsync());
+        }
+
+    }
+}
